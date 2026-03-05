85fa02b397c20c6d0994896c4b394345e4fef917
Joe Kim <joesm.genie@gmail.com>
2026-03-05 11:11:26 +0700

    add example page route

---
+++ src/routes/hi/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { resolve } from '$app/paths';
+</script>
+
+<svelte:head>
+  <title>Hi</title>
+  <meta name="description" content="Hi page" />
+</svelte:head>
+
+<div class="text-column">
+  <h1>Hi there!</h1>
+
+  <p>
+    This is the hi page. You can navigate to the <a href={resolve('/about')}>about page</a> to learn more about this app.
+  </p>
+</div>
